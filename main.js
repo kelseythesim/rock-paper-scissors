09fe23f68a559ca7d6355fb7c4805d2661bf84a4
Kelsey <kelseythesim@gmail.com>
2023-04-08 11:18:50 -0700

    new function for the game

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ function getComputerChoice () {
     let result = myArray[Math.floor(Math.random() * myArray.length)];
     return result;
 };
+
+function playGame (computerSelection, playerSelection) {
+    if (computerSelection = "rock" && playerSelection === "scissors") {
+        return "You lose. Rock beats scissors!"
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You win! Rock beats scissors!"
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You lose! Paper beats rock!"
+    } else if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You win! Paper beats rock!"
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You win! Scissors beats paper!"
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You lose! Scissors beats paper!"
+    } else {
+        return "It's a tie! Try again."
+    }
+}
